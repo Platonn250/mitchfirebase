4502e33c4c7041099833d79e58d37ea7f911c0bf
unknown <platonn250@gmail.com>
2022-08-02 10:30:43 +0200

    login page

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,12 +1,125 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // hello again
+              Text(
+                "Hello World",
+                style: GoogleFonts.bebasNeue(fontSize: 54),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              // email textfeild
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Center(
+                      child: Text(
+                    "SignInn",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Dont Have an account? ",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(" Register now",
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold))
+                ],
+              )
+
+              // sing in but
+
+              // not a member
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
